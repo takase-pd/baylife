9f49fc4de0f6e322cd8188287649ba2f5cd57af6
takase-pd <63141701+takase-pd@users.noreply.github.com>
2021-08-13 14:47:07 +0900

    Update exe_local.sh

--- exe_local.sh
+++ exe_local.sh
@@ -5,4 +5,4 @@ git merge origin/flutterflow
 # Flutter compile
 flutter pub get
 flutter packages pub run build_runner build --delete-conflicting-outputs
-flutter run -d iPhone\ Xs
+flutter run -d chrome --web-port 5000
