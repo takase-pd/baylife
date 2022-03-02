f71271bb17a8e97dd918972b32c9f45bcecd436a
Yuji Takase <yuji.takase@particledrawing.com>
2022-03-02 23:57:44 +0900

    Update for Flutter 2.10.2

--- exe_local.sh
+++ exe_local.sh
@@ -3,7 +3,9 @@
 # git merge origin/flutterflow
 
 # Flutter compile
-fvm flutter pub get
+# fvm flutter pub get
+fvm flutter clean
+fvm flutter pub upgrade
 fvm flutter packages pub run build_runner build --delete-conflicting-outputs
 # fvm flutter run -d iPhone\ Xs
 fvm flutter run
