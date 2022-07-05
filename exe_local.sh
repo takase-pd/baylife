0860da58431121a83db4909b2773f5db055ddbbe
Yuji Takase <yuji.takase@particledrawing.com>
2022-07-05 17:01:16 +0900

    Update to fix a size of text

--- exe_local.sh
+++ exe_local.sh
@@ -6,7 +6,7 @@
 # fvm flutter pub get
 fvm flutter clean
 # fvm flutter pub cache repair
-rm -rf pubspec.lock ios/Pods ios/Podfile.lock
+# rm -rf pubspec.lock ios/Pods ios/Podfile.lock
 fvm flutter pub upgrade
 fvm flutter packages pub run build_runner build --delete-conflicting-outputs
 # fvm flutter run -d iPhone\ Xs
